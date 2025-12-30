051383ec45ba00f67fb2d8e3abda4eeefc2b3b59
ts-geek75 <labdhi.shah@techstaunch.com>
2025-12-30 11:56:36 +0530

    Solved build error

--- src/app/(pagegroup)/admin/bookings/page.tsx
+++ src/app/(pagegroup)/admin/bookings/page.tsx
@@ -1,7 +1,28 @@
-import BookingPanel from '@/modules/bookings/BookingPanel';
+"use client";
 
-const Bookingpage = () => {
-  return <BookingPanel/>
+import React, { useState } from "react";
+
+import BookingCalendar from "@/modules/bookings/BookingCalender";
+import CreateBookingDialog from "@/modules/bookings/components/CreateBooking";
+
+const Bookingpage: React.FC = () => {
+  const [isBookingPanelOpen, setIsBookingPanelOpen] = useState(false);
+  const [view, setView] = useState<"calendar" | "table">("calendar");
+
+  return (
+    <>
+      <BookingCalendar
+        setIsBookingPanelOpen={setIsBookingPanelOpen}
+        view={view}
+        setView={setView}
+      />
+
+      <CreateBookingDialog
+        open={isBookingPanelOpen}
+        onOpenChange={setIsBookingPanelOpen}
+      />
+    </>
+  );
 }
 
-export default Bookingpage; 
+export default Bookingpage ;
